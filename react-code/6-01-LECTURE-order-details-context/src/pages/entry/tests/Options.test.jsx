10e14fe2700fe8e4cb06e712afc32089c3242a2d
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-11-27 20:17:35 +0200

    removed unnessesary files

--- react-code/6-01-LECTURE-order-details-context/src/pages/entry/tests/Options.test.jsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
-import Options from "../Options";
-
-test("displays image for each scoop option from server", async () => {
- 
-});
--- react-code/6-03-QUIZ-grand-total/src/pages/entry/tests/OrderEntry.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
-import { HttpResponse, http } from "msw";
-import { server } from "../../../mocks/server";
-import OrderEntry from "../OrderEntry";
-
-test("handles error for scoops and toppings routes", async () => {
-  
-});
