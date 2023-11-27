44ff55f7c99692695693cc92d58870981e7b53a5
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-11-27 20:15:14 +0200

    added overlay, replaced fireEvent with userEvent and used hover

--- sundae-starter/src/components/SummaryForm.jsx
+++ sundae-starter/src/components/SummaryForm.jsx
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function SummaryForm() {
     const [isChecked, setIsChecked] = useState(false);
+
+    const popover = (
+        <Popover id="popover">
+          <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+        </Popover>
+    );
+
+    const checkboxLabel = (
+        //the default value of the trigger property is mouseover
+        <span>I agree to
+            <OverlayTrigger placement="right" overlay={popover}>
+                <span style={{ color: "blue" }}> Terms and Conditions</span>
+            </OverlayTrigger>
+        </span>
+    )
 
     return(
         <Form>
             <Form.Group controlId="terms-and-conditions">
                 <Form.Check
                     type="checkbox"
-                    id="terms&condintions"
-                    name="terms-and-conditions"
-                    value={isChecked} 
+                    checked={isChecked} 
                     onClick={() => setIsChecked(!isChecked)}
+                    label={checkboxLabel}
                 />
-                <label htmlFor="terms&condintions">I agree to Terms and Conditions</label>
             </Form.Group>
             <Button type="submit" disabled={!isChecked}>Confirm order</Button>
         </Form>
--- sundae-starter/src/components/tests/OrderSummary.test.jsx
+++ sundae-starter/src/components/tests/OrderSummary.test.jsx
@@ -1,7 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
-test("checkbox enables button flow", () => {
+test("checkbox enables button flow", async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
     //checkbox is unchecked by default (initial state)
     const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i});
@@ -11,12 +14,31 @@ test("checkbox enables button flow", () => {
 
 
     //checkbox enables the submit order button
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(submitButton).toBeEnabled();
     expect(checkbox).toBeChecked();
 
     //unchecking the checkbox again disables the submit order button
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(submitButton).toBeDisabled();
     expect(checkbox).not.toBeChecked();
+});
+
+test("popover response to hover", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm/>);
+
+    // popover should be hidden by default
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+
+    //popover should appear when we hover/mouseover over the checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    //popover should disappear when we mouseout
+    await user.unhover(popover);
+    expect(popover).not.toBeInTheDocument();
 });
