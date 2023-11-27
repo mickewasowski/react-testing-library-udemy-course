5ea40248195e34e0bc5a0a9b977ec3a01d96b72f
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-11-27 20:18:12 +0200

    removed unnessesary files

--- react-code/5-03-LECTURE-options-alert-banner/src/pages/entry/tests/Options.test.jsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Options from "../Options";
-
-test("displays image for each scoop option from server", async () => {
- 
-});
--- react-code/8-04-QUIZ-no-subtotal-update-invalid-count/src/pages/entry/tests/ScoopOption.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
-import userEvent from "@testing-library/user-event";
-import ScoopOption from "../ScoopOption";
-
-test("indicate if scoop count is non-int or out of range", async () => {
-  
-});
