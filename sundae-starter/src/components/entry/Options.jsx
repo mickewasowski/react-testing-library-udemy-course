1b192ecc8fe4c2d38cd5e93ae16a4b30433b6123
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-11-30 23:35:32 +0200

    testing async population of data in a component

---
+++ sundae-starter/src/components/entry/Options.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ScoopOption from './ScoopOption';
+import Row from 'react-bootstrap/Row';
+
+export default function Options({optionType}) {
+    const [items, setItems] = useState([]);
+
+    //optionType is either 'scoops' or 'toppings'
+    useEffect(() => {
+        axios.get(`http://localhost:3030/${optionType}`)
+            .then(response => setItems(response.data))
+            .catch(error => console.error(error));
+    }, [optionType]);
+
+    // TODO: replace null with ToppingOption when available
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+    const optionItems = items.map(item => {
+        return <ItemComponent
+            key={item.name}
+            name={item.name}
+            imagePath={item.imagePath}
+            />
+    })
+
+    return(
+        <Row>
+            {optionItems}
+        </Row>
+    )
+}
