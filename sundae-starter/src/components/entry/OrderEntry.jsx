942f7abb1acca0ffcff77c66fa3b4589aa3e8d4e
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-03 21:05:31 +0200

    code quiz: grand total tests and react code implemented

--- sundae-starter/src/components/entry/OrderEntry.jsx
+++ sundae-starter/src/components/entry/OrderEntry.jsx
@@ -1,11 +1,16 @@
-import Options from "./Options"
+import Options from "./Options";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from '../../utilities/index';
 
 export default function OrderEntry() {
+    const { totals } = useOrderDetails();
+    const grandTotal = formatCurrency(totals.scoops + totals.toppings);
 
     return (
         <div>
             <Options optionType='scoops' />
             <Options optionType='toppings' />
+            <h2>Grand total: {grandTotal}</h2>
         </div>
     )
 }
--- sundae-starter/src/components/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/components/entry/tests/totalUpdates.test.jsx
@@ -2,6 +2,7 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import OrderEntry from '../OrderEntry';
 
 test('update scoop subtotal when scoops change', async () => {
     const user = userEvent.setup();
@@ -46,4 +47,71 @@ test('update topping subtotal when topping is checked', async () => {
 
     await user.click(cherriesCheckbox);
     expect(toppingsSubtotal).toHaveTextContent('1.50');
+});
+
+describe('grand total', () => {
+    test('grand total starts at $0.00', () => {
+        render(<OrderEntry />);
+
+        const grandTotal = screen.getByText('Grand total: $', { exact: false });
+        expect(grandTotal).toHaveTextContent('0.00');
+    });
+
+    test('grand total updates properly if scoop is added first', async () => {
+        const user = userEvent.setup();
+        render(<OrderEntry />);
+
+        const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+        const chocolateInput = await screen.findByRole('spinbutton', { name: 'Chocolate' });
+        await user.clear(chocolateInput);
+        await user.type(chocolateInput, '1');
+        expect(grandTotal).toHaveTextContent('2.00');
+
+        const hotFundgeTopping = await screen.findByRole('checkbox', { name: 'Hot fudge' });
+        await user.click(hotFundgeTopping);
+        expect(grandTotal).toHaveTextContent('3.50');
+    });
+
+    test('grand total updates properly if topping is added first', async () => {
+        const user = userEvent.setup();
+        render(<OrderEntry />);
+
+        const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+        const gummiBearsTopping = await screen.findByRole('checkbox', { name: 'Gummi bears' });
+        await user.click(gummiBearsTopping);
+        expect(grandTotal).toHaveTextContent('1.50');
+
+        const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+        await user.clear(vanillaInput);
+        await user.type(vanillaInput, '1');
+        expect(grandTotal).toHaveTextContent('3.50');
+    });
+
+    test('grand total updates properly if an item is removed', async () => {
+        const user = userEvent.setup();
+        render(<OrderEntry />);
+
+        const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+        const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+        await user.clear(vanillaInput);
+        await user.type(vanillaInput, '1');
+        expect(grandTotal).toHaveTextContent('2.00');
+
+        const mochiTopping = await screen.findByRole('checkbox', { name: 'Mochi' });
+        await user.click(mochiTopping);
+        expect(grandTotal).toHaveTextContent('3.50');
+
+        const cherriesTopping = await screen.findByRole('checkbox', { name: 'Cherries' });
+        await user.click(cherriesTopping);
+        expect(grandTotal).toHaveTextContent('5.00');
+
+        await user.click(mochiTopping);
+        expect(grandTotal).toHaveTextContent('3.50');
+
+        await user.type(vanillaInput, '0');
+        expect(grandTotal).toHaveTextContent('1.50');
+    });
 });
