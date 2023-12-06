4fca68dda7581d3a4ff8c8970de4557f4a933d7d
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-06 21:04:09 +0200

    red box if invalid value is provided for scoop count

--- sundae-starter/src/components/entry/ScoopOption.jsx
+++ sundae-starter/src/components/entry/ScoopOption.jsx
@@ -2,11 +2,18 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { useOrderDetails } from '../../contexts/OrderDetails';
+import { useState } from 'react';
 
 export default function ScoopOption({name, imagePath}) {
     const { updateItemCount } = useOrderDetails();
+    const [isInvalidInput, setIsInvalidInput] = useState(false);
 
     const handleChange = (event) => {
+        const currentInput = parseFloat(event.target.value);
+        const valueIsValid = 0 <= currentInput && currentInput <= 10 &&
+        Math.floor(currentInput) === currentInput;
+        setIsInvalidInput(!valueIsValid);
+
         updateItemCount(name, parseInt(event.target.value), "scoops")
     }
 
@@ -24,6 +31,7 @@ export default function ScoopOption({name, imagePath}) {
                         type="number"
                         defaultValue={0}
                         onChange={handleChange}
+                        isInvalid={isInvalidInput}
                     />
                 </Col>
             </Form.Group>
